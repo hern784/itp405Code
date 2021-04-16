6e89275f4f8c6234f293a2a0068e25b9d16f6968
Richard Hernandez <rhdezc12@gmail.com>
2021-04-15 17:58:59 -0700

    assignment 10

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -24,12 +24,6 @@ class RegistrationController extends Controller
         $user->email = $request->input('email');
         $user->password = Hash::make($request->input('password')); //bcrypt
 
-        $userRole = Role::getUser();
-        $user->role()->associate($userRole);
-
-        $user->save();
-
-        Auth::login($user);
-        return redirect()->route('profile.index');
+        print($user->password);
     }
 }
